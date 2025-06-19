08fe410d962f5e2014db41117a235701bd67bc10
AInoriex <alexinori@163.com>
2025-06-19 11:30:04 +0800

    [feat] 补充路径初始化

--- src/stream/main.go
+++ src/stream/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"eshop_server/src/stream/handler"
+	"eshop_server/src/stream/model"
 	"eshop_server/src/utils/config"
+
 	// "eshop_server/src/utils/db"
 	"eshop_server/src/utils/log"
 	"fmt"
@@ -25,6 +27,12 @@ func main() {
 	// TODO 初始化数据库
 	// db.InitMysqlAll(config.DbConfig.Mysql.Host, config.DbConfig.Mysql.Db, config.DbConfig.Mysql.MaxCon, db.Con_Main, config.CommonConfig.OpenDbLog)
 	// log.Info("初始化Mysql数据库成功")
+
+	// 初始化音频路径
+	if err = model.InitStreamingPaths(); err != nil {
+		log.Error("初始化项目路径失败")
+		return
+	}
 
 	// 初始化路由
 	handler.InitRouter()
--- src/stream/model/common.go
+++ src/stream/model/common.go
@@ -1,6 +1,22 @@
 package model
 
-const (
-	StreamFileUploadPath string = "uploads"
-	StreamFileSegmentPath string = "segments"
+import (
+	"os"
+	"path/filepath"
 )
+
+const (
+	StreamFileUploadPath string = "uploads" // 音频上传路径
+	StreamFileSegmentPath string = "segments" // 音频切片路径
+)
+
+// 初始化路径
+func InitStreamingPaths() error {
+	if err := os.MkdirAll(filepath.Dir(StreamFileUploadPath), 0750); err != nil {
+		return err
+	}
+	if err := os.MkdirAll(filepath.Dir(StreamFileSegmentPath), 0750); err != nil {
+		return err
+	}
+	return nil
+}
