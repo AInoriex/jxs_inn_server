e57b064debcc2cbccf63870d6ddc48cdbaf493da
AInoriex <alexinori@163.com>
2025-08-15 20:40:25 +0800

    [feat] 新增飞书告警重试逻辑

--- src/utils/alarm/lark_robot.go
+++ src/utils/alarm/lark_robot.go
@@ -3,12 +3,14 @@ package alarm
 import (
 	"bytes"
 	"encoding/json"
+	"eshop_server/src/utils/utime"
 	"fmt"
 	"net/http"
 )
 
 // 飞书告警文本信息
 func PostFeiShu(level, url string, info string) error {
+	var retryCount int = 3
 	msg := FeiShuMsg{
 		MsgType: "text",
 	}
@@ -17,13 +19,25 @@ func PostFeiShu(level, url string, info string) error {
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequest("POST", url, bytes.NewReader(marshal))
-	if err != nil {
-		return err
+
+	// 包含初始请求的总重试循环（初始请求+retryCount次重试）
+	for i := 0; i <= retryCount; i++ {
+		req, err := http.NewRequest("POST", url, bytes.NewReader(marshal))
+		if err != nil {
+			return err // 请求创建失败无法重试，直接返回错误
+		}
+		resp, err := http.DefaultClient.Do(req)
+		defer func() {
+			if resp != nil { _ = resp.Body.Close() }
+		}()
+		// 请求成功
+		if err == nil {
+			return nil
+		}
+		// 请求失败尝试重试
+		if i < retryCount {
+			utime.RandomSleep(3, 5)
+		}
 	}
-	_, err = http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	return nil
+	return fmt.Errorf("请求失败，已尝试%d次", retryCount + 1)
 }
